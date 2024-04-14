9f30c9e6b1a69df5ab337499fbe339affecd9325
praparma <praniti.parmar@publicissapient.com>
2024-04-14 20:55:33 +0530

    Episode 1

---
+++ Episode 1/App.js
@@ -0,0 +1,37 @@
+const heading = React.createElement(
+  "h1",
+  { id: "title" },
+  "Hello I am Learing React"
+);
+
+//Example of the Nested Html structure inside the React Application
+const parent = React.createElement(
+  "parent",
+  { id: "parent" },
+  React.createElement("child", { id: "child" }),
+  [
+    React.createElement("h1", { id: "title" }, "Ema the h1 tag"),
+    React.createElement("h3", {}, "Ema the h3 tag!"),
+  ] //This is the array of children
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(parent);
+
+// Render = React element is a JavaScript Object which after using the keyword render will be converted to HTML
+// that the Browser understands
+
+//Creating 2 children under the same parent
+const parentMum = React.createElement("parentMum", {}, [
+  React.createElement("child1", {}, [
+    React.createElement("h1", {}, "ema the h1 here"),
+    React.createElement("h2", {}, "me the h2"),
+  ]),
+  React.createElement("child2", {}, [
+    React.createElement("h3", {}, "me the h3"),
+    React.createElement("h4", {}, "me the h4"),
+  ]),
+]);
+
+root.render(parentMum);
