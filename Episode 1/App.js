2f0be3e915a688f29d26d7b1f4dca15a1102a600
praparma <praniti.parmar@publicissapient.com>
2024-04-20 13:30:58 +0530

    episode 1

--- Episode 1/App.js
+++ Episode 1/App.js
@@ -35,3 +35,35 @@ const parentMum = React.createElement("parentMum", {}, [
 ]);
 
 root.render(parentMum);
+
+// function x() {
+//   console.log("Nmamaste JavaScript");
+//   for (let i = 1; i <= 5; i++) {
+//     setTimeout(function () {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+//   console.log("Nmamaste JavaScript");
+// }
+// x();
+
+// function x() {
+//   for (var i = 1; i <= 5; i++) {
+//     function close(x) {
+//       setTimeout(function () {
+//         console.log(x);
+//       }, i * 1000);
+//     }
+//     close(i);
+//   }
+//   console.log("Hi Iam Here");
+// }
+// x();
+
+function x(y) {
+  y();
+  console.log("iam in x");
+}
+x(function y() {
+  console.log("Hi Iam inside the y function");
+});
